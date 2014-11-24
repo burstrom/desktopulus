feee8f37931549f0d47025df438ca9d1c814f7d7
laboration comp <zakarias.juto@zakjut.com>
2014-11-24 11:17:57 +0100

    [Commenting]
    Added comments to the initial code. Should be easier to understand now.

--- Unity/unity-demo/Assets/TestScripts/CreateScreen.cs
+++ Unity/unity-demo/Assets/TestScripts/CreateScreen.cs
@@ -9,24 +9,29 @@ public class CreateScreen : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButtonDown("CreateScreen")){
-			
+			//Radius, this is the distance between the actor and the screens.
 			float radius = 8.0f;
+			//This is the multiplier for placing screens in a multiplier*pi half circle around the actor. Starts at 0.5 to place directly in front.
 			float multiplier = 0.5f;
 			
 			//Initial location
 			float z = radius * Mathf.Sin(multiplier*Mathf.PI);
 			float x = radius * Mathf.Cos(multiplier*Mathf.PI);
-			float height = 5.0f; //Does not need to be changed.
-
+			float height = 5.0f; //Does not need to be changed unless you also change the initial position of the actor.
 			Vector3 startPos = new Vector3(x, height, z);
 			
-			//Detects collisions between game objects, returns an array with all objects colliding at a position.
+			//Detects collisions between game objects, returns an array with all objects colliding at a position. The float is the
+			//sensitivity of the sphere, basically it needs to be slightly larger than the screens width.
 			var collision = Physics.OverlapSphere(startPos, 5.5f);
 			
+			//Prevent infinite loops if no position can be found to add a screen.
 			bool looped = false;
+			
+			//What direction are we adding new objects, swaps between -1 and 1.
 			float sideDirection = 1.0f;
 			float vertDirection = 1.0f;
-			//Recalculate game position, something goes wrong here.
+			
+			//Try new positions to add the screen while a collision is detected.
 			while (collision.Length > 0){
 				if(collision[0] == Target){
 					Debug.Log(multiplier);
@@ -34,7 +39,8 @@ public class CreateScreen : MonoBehaviour {
 				}
 				multiplier += 0.05f*sideDirection;
 				Debug.Log(multiplier);
-				//catch the base case when you reach 1 Pi, loop around to other side.
+				//catch the base case when you reach 1 Pi, prevents screens from being spawned behind the actor.
+				//If it can spawn no more at a height-level it alternatingly jumps above and below the actor to spawn.
 				if (multiplier > 1.0f || multiplier < 0.0f){
 					if(looped){
 						startPos.y += 8.0f*vertDirection;
@@ -52,7 +58,7 @@ public class CreateScreen : MonoBehaviour {
 				collision = Physics.OverlapSphere(startPos, 5.5f);
 			}
 			
-			
+			//Actually create the screen.
 			GameObject newscreen = Instantiate(Screen, startPos, transform.rotation) as GameObject;
 			newscreen.GetComponent<FaceThis>().target = Target;
 		}
