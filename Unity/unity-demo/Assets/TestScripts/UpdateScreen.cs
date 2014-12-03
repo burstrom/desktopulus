4b49eee21f4c024afdb428265122c752c83ad1c4
laboration comp <zakarias.juto@zakjut.com>
2014-12-03 17:07:36 +0100

    [FIX] Now working from memory instead of disk when loading bytearrays.

--- Unity/unity-demo/Assets/TestScripts/UpdateScreen.cs
+++ Unity/unity-demo/Assets/TestScripts/UpdateScreen.cs
@@ -2,69 +2,72 @@
 using System.Collections;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
+using System.IO;
 
 
 
+public class UpdateScreen : MonoBehaviour
+{
 
-public class UpdateScreen : MonoBehaviour {
-
-	private static System.Drawing.Bitmap bmpScreenshot;
-	private static System.Drawing.Graphics gfxScreenshot;
+		private static System.Drawing.Bitmap bmpScreenshot;
+		private static System.Drawing.Graphics gfxScreenshot;
+		private System.IO.MemoryStream memStream;
+		private int height;
+		private int width;
 	
-	IEnumerator updateTexture(){
-		WWW www = new WWW("file://Screenshot.png");
-		yield return www;
-		if (renderer.material.mainTexture != null)
-			DestroyImmediate(renderer.material.mainTexture, true);
-			
-		renderer.material.mainTexture = www.texture;
 		
-		www.Dispose();
-	}
+		// Use this for initialization
+		void Start ()
+		{
 	
-	// Use this for initialization
-	void Start () {
+		}
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// Use System.Window.Forms to access the screen you want to capture. Capture a bitmap of that screen.
-		//Right now we capture the primary screen all the time but using AllScreens[i] we can access all the screens.
-		try{
-			bmpScreenshot = new System.Drawing.Bitmap(System.Windows.Forms.Screen.AllScreens[GetComponent<updateObject>().screenNum].Bounds.Width, 
-			                                          System.Windows.Forms.Screen.AllScreens[GetComponent<updateObject>().screenNum].Bounds.Height, 
+		// Update is called once per frame
+		void Update ()
+		{
+				// Use System.Window.Forms to access the screen you want to capture. Capture a bitmap of that screen.
+				//Right now we capture the primary screen all the time but using AllScreens[i] we can access all the screens.
+				try {
+						width = System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].Bounds.Width;
+						height = System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].Bounds.Height;
+				
+						bmpScreenshot = new System.Drawing.Bitmap (width, height, 
 			                                          PixelFormat.Format32bppArgb);
 			                                          
-			gfxScreenshot = System.Drawing.Graphics.FromImage(bmpScreenshot);
+						gfxScreenshot = System.Drawing.Graphics.FromImage (bmpScreenshot);
 	
-			gfxScreenshot.CopyFromScreen(System.Windows.Forms.Screen.AllScreens[GetComponent<updateObject>().screenNum].Bounds.X,
-			                             System.Windows.Forms.Screen.AllScreens[GetComponent<updateObject>().screenNum].Bounds.Y,
+						gfxScreenshot.CopyFromScreen (System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].Bounds.X,
+			                             System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].Bounds.Y,
 			                             0,
 			                             0,
-			                             System.Windows.Forms.Screen.AllScreens[GetComponent<updateObject>().screenNum].Bounds.Size,
+			                             System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].Bounds.Size,
 			                             System.Drawing.CopyPixelOperation.SourceCopy);
-			//The following two steps, we want to skip, basically we want to apply the texture directly wihtout saving it first.
+						//The following two steps, we want to skip, basically we want to apply the texture directly wihtout saving it first.
 			
-			//Convert the bitmap image into an "actual" image.
-			gfxScreenshot.Dispose();
-			bmpScreenshot.Save("Screenshot.png", ImageFormat.Png);
-		Debug.Log(System.Windows.Forms.Screen.AllScreens[GetComponent<updateObject>().screenNum].WorkingArea);
+						//Convert the bitmap image into an "actual" image.
+						gfxScreenshot.Dispose ();
+						byte[] barr;
+			            
+						
+						memStream = new System.IO.MemoryStream ();
+						bmpScreenshot.Save (memStream, System.Drawing.Imaging.ImageFormat.Png);
+						memStream.Close();
+						barr = memStream.ToArray();
 			
-			//Get rid of the screenshots from memory.
-			bmpScreenshot.Dispose();
-			StartCoroutine(updateTexture());
 			
-		} catch (System.IndexOutOfRangeException e){
-			Debug.Log ("Can not update screen, has no valid input.");
+						//Get rid of the screenshots from memory.
+						bmpScreenshot.Dispose ();
+						var texture = new Texture2D (width, height, TextureFormat.ARGB32, false); 
+						if (renderer.material.mainTexture != null) {
+								DestroyImmediate (renderer.material.mainTexture, true);
+						}
+						texture.LoadImage(barr);
+						renderer.material.mainTexture = texture;
+			
+				} catch (System.IndexOutOfRangeException e) {
+						Debug.Log ("Can not update screen, has no valid input:" + e);
+				}
+				
 		}
-		
-		
-		
-		
-		
-		
-		
-	}
 	
 }
