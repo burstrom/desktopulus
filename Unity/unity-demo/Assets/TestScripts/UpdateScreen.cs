e1bd8142c9466c66162758f92983efd66c42f3fa
laboration comp <zakarias.juto@zakjut.com>
2014-12-08 15:28:49 +0100

    testing more screens

--- Unity/unity-demo/Assets/TestScripts/UpdateScreen.cs
+++ Unity/unity-demo/Assets/TestScripts/UpdateScreen.cs
@@ -14,6 +14,8 @@ public class UpdateScreen : MonoBehaviour
 		private System.IO.MemoryStream memStream;
 		private int height;
 		private int width;
+		private int screenNum;
+		
 	
 		
 		// Use this for initialization
@@ -25,46 +27,50 @@ public class UpdateScreen : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{		
-				
+				Debug.Log (System.Windows.Forms.Screen.AllScreens.Length);
 				// Use System.Window.Forms to access the screen you want to capture. Capture a bitmap of that screen.
-				//Right now we capture the primary screen all the time but using AllScreens[i] we can access all the screens.
+				// XXXX For some reason, AllScreens always only has one object in it. 
 				try {
-						Debug.Log (GetComponent<updateObject> ().screenNum);
-						//width = System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].Bounds.Width;
-						//height = System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].Bounds.Height;
-						width = System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].WorkingArea.Width;
-						height = System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].WorkingArea.Height;
+						screenNum = GetComponent<updateObject>().screenNum;
+						width = System.Windows.Forms.Screen.AllScreens[screenNum].WorkingArea.Width;
+						height = System.Windows.Forms.Screen.AllScreens[screenNum].WorkingArea.Height;
 				
-						bmpScreenshot = new System.Drawing.Bitmap (width, height, 
-			                                          PixelFormat.Format32bppArgb);
-			                                          
+						//Create the bitmap that will store the information from the screenbuffer.
+						bmpScreenshot = new System.Drawing.Bitmap (width, height, PixelFormat.Format32bppArgb);
+			               
+			            //Create the graphics item that will perform the copy.                                                         
 						gfxScreenshot = System.Drawing.Graphics.FromImage (bmpScreenshot);
 	
-						gfxScreenshot.CopyFromScreen (System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].WorkingArea.X,
-			                             System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].WorkingArea.Y,
+						//Copy the screen into the bitmap.
+						gfxScreenshot.CopyFromScreen (System.Windows.Forms.Screen.AllScreens[screenNum].WorkingArea.X,
+			                             System.Windows.Forms.Screen.AllScreens[screenNum].WorkingArea.Y,
 			                             0,
 			                             0,
-			                             System.Windows.Forms.Screen.AllScreens [GetComponent<updateObject> ().screenNum].WorkingArea.Size,
+			                             System.Windows.Forms.Screen.AllScreens[screenNum].WorkingArea.Size,
 			                             System.Drawing.CopyPixelOperation.SourceCopy);
-						//The following two steps, we want to skip, basically we want to apply the texture directly wihtout saving it first.
 			
-						//Convert the bitmap image into an "actual" image.
 						gfxScreenshot.Dispose ();
+						
+						//Create a bytearray, we can't convert a bitmap directly into a texture. But we can flush it into a byte array that can then
+						//be converted to a texture.
 						byte[] barr;
 			            
-						
+			            //access the memory stream and flush it into the byte array.
 						memStream = new System.IO.MemoryStream ();
 						bmpScreenshot.Save (memStream, System.Drawing.Imaging.ImageFormat.Png);
 						memStream.Close();
+						bmpScreenshot.Dispose();
 						barr = memStream.ToArray();
 			
-			
-						//Get rid of the screenshots from memory.
-						bmpScreenshot.Dispose ();
-						var texture = new Texture2D (width, height, TextureFormat.ARGB32, false); 
+						//Create the new texture.
+						var texture = new Texture2D (width, height, TextureFormat.ARGB32, false);
+						
+						//Destroy the old texture, will cause a memory leak otherwise.  
 						if (renderer.material.mainTexture != null) {
 								DestroyImmediate (renderer.material.mainTexture, true);
 						}
+						
+						//Set the new texture as the main one.
 						texture.LoadImage(barr);
 						renderer.material.mainTexture = texture;
 			
