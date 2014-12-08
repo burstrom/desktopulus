d5a5113597ef36557c424959cb60451f53cad9f3
laboration comp <zakarias.juto@zakjut.com>
2014-12-08 13:20:56 +0100

    testing shit

--- Unity/unity-demo/Assets/TestScripts/UpdateScreen.cs
+++ Unity/unity-demo/Assets/TestScripts/UpdateScreen.cs
@@ -24,7 +24,12 @@ public class UpdateScreen : MonoBehaviour
 	
 		// Update is called once per frame
 		void Update ()
-		{
+		{		
+				string info = "";
+				foreach(var screen in System.Windows.Forms.Screen.AllScreens){
+					info = info + "Working Area: " + screen.WorkingArea.ToString() + "Primary Screen: " + screen.Primary.ToString() + "\n";
+					Debug.Log (info);
+				}
 				// Use System.Window.Forms to access the screen you want to capture. Capture a bitmap of that screen.
 				//Right now we capture the primary screen all the time but using AllScreens[i] we can access all the screens.
 				try {
