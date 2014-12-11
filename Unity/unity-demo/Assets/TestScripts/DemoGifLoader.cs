f5499518695b049555df52d66f4b62cfd5277714
laboration comp <zakarias.juto@zakjut.com>
2014-12-11 14:32:20 +0100

    [DEMO2] demo branch updated

---
+++ Unity/unity-demo/Assets/TestScripts/DemoGifLoader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+public class DemoGifLoader : MonoBehaviour {
+
+	public static List<List<Texture2D>> gFrames = new List<List<Texture2D>>();
+	
+	void Start () {
+		for(int j = 0; j<9; j++){
+		List<Texture2D> gifFrames = new List<Texture2D>();
+		gFrames.Add(gifFrames);
+			string loadingGifPath = Application.dataPath + "/TestScripts/Materials/"+ j +".gif";
+			var gifImage = Image.FromFile (loadingGifPath);
+			var dimension = new FrameDimension (gifImage.FrameDimensionsList [0]);
+			int frameCount = gifImage.GetFrameCount (dimension);
+			for (int i = 0; i < frameCount; i++) {
+				gifImage.SelectActiveFrame (dimension, i);
+				var frame = new Bitmap (gifImage.Width, gifImage.Height);
+				System.Drawing.Graphics.FromImage (frame).DrawImage (gifImage, Point.Empty);
+				Texture2D frameTexture = new Texture2D (frame.Width, frame.Height);
+				for (int x = 0; x < frame.Width; x++)
+				for (int y = 0; y < frame.Height; y++) {
+					System.Drawing.Color sourceColor = frame.GetPixel (x, y);
+					frameTexture.SetPixel (x, frame.Height - 1 - y, new Color32 (sourceColor.R, sourceColor.G, sourceColor.B, sourceColor.A)); // for some reason, x is flipped
+				}
+				frameTexture.Apply ();
+				gFrames[j].Add(frameTexture);
+				//gifFrames.Add (frameTexture);
+			}
+		}
+	}
+}
--- Unity/unity-demo/Assets/TestScripts/DemoScreen.cs
+++ Unity/unity-demo/Assets/TestScripts/DemoScreen.cs
@@ -4,47 +4,31 @@ using System.Drawing.Imaging;
 using UnityEngine;
 
 public class DemoScreen : MonoBehaviour
-{
-		public string loadingGifPath = "/Assets/TestScripts/Material/owls-yousrs.gif";
+{	
+		
+		//private string loadingGifPath = Application.dataPath + "/TestScripts/Materials/owls-yousrs.gif";
 		public float speed = 1;
 		private int frame = 0;
 		public int framecount = 0;
 		private int count = 0;
 		public Vector2 drawPosition;
 		private Texture2D frameTexture;
-	
-		List<Texture2D> gifFrames = new List<Texture2D> ();
-		void Start ()
-		{
-				var gifImage = Image.FromFile (loadingGifPath);
-				var dimension = new FrameDimension (gifImage.FrameDimensionsList [0]);
-				int frameCount = gifImage.GetFrameCount (dimension);
-				for (int i = 0; i < frameCount; i++) {
-						gifImage.SelectActiveFrame (dimension, i);
-						var frame = new Bitmap (gifImage.Width, gifImage.Height);
-						System.Drawing.Graphics.FromImage (frame).DrawImage (gifImage, Point.Empty);
-						frameTexture = new Texture2D (frame.Width, frame.Height);
-						for (int x = 0; x < frame.Width; x++)
-								for (int y = 0; y < frame.Height; y++) {
-										System.Drawing.Color sourceColor = frame.GetPixel (x, y);
-										frameTexture.SetPixel (x, frame.Height - 1 - y, new Color32 (sourceColor.R, sourceColor.G, sourceColor.B, sourceColor.A)); // for some reason, x is flipped
-								}
-						frameTexture.Apply ();
-						gifFrames.Add (frameTexture);
-				}
+		private int snum; 
+		void Start(){
+			snum = GetComponent<updateObject>().screenNum;
 		}
 	
 		void Update ()
 		{				
-				renderer.material.mainTexture = gifFrames [frame];
-				if (count != framecount) {
-						count++;
-				} else {
-						frame++;
-						if (frame == gifFrames.Count) {
-								frame = 0;
-						}
-						count = 0;
+			renderer.material.mainTexture = DemoGifLoader.gFrames[snum % 9][frame];
+			if (count != framecount) {
+				count++;
+			} else {
+				frame++;
+				if (frame == DemoGifLoader.gFrames[snum % 9].Count) {
+						frame = 0;
 				}
+				count = 0;
+			}
 		}
 }
