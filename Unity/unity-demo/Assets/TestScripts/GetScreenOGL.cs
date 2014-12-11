014beb7d04c433685cb31b01ebf13b6a760233cf
Kenneth Kullbrandt <kenneth@kullbrandt.se>
2014-12-11 11:24:37 +0100

    [DEMO] Added jiff support

---
+++ Unity/unity-demo/Assets/TestScripts/GetScreenOGL.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetScreenOGL : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
